64f4fd2d09841656148c06f46525dc410f55ca94
castrodrigo <castrodrigo@yahoo.com.br>
2019-04-02 00:22:27 +0200

    Add basic packages and infra

---
+++ screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+import styled from "styled-components";
+
+const Container = styled.View`
+  flex: 1;
+  padding-top: 30px;
+`;
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  render() {
+    return (
+      <Container>
+        <ScrollView>
+          <Text>Home</Text>
+        </ScrollView>
+      </Container>
+    );
+  }
+}
