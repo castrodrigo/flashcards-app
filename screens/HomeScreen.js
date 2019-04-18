b5f23c262f4556a85a464fa2a4b22f04f9864d51
castrodrigo <castrodrigo@yahoo.com.br>
2019-04-18 10:11:29 +0200

    Setup basic tests to run Jest

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ScrollView, Text } from "react-native";
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/native";
 import { ExpoLinksView } from "@expo/samples";
 
 const Container = styled.ScrollView`
