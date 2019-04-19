44a60feeab2719b073a9019f1b07b85c86ccec45
castrodrigo <castrodrigo@yahoo.com.br>
2019-04-19 23:08:13 +0200

    Mock Deck List View on home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ScrollView, Text } from "react-native";
 import styled from "styled-components/native";
 import Header from "../components/Header";
+import DeckListItem from "../components/DeckListItem";
 
 const Container = styled.View`
   flex: 1;
@@ -15,7 +16,9 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <Container>
-        <ScrollView />
+        <ScrollView>
+          <DeckListItem />
+        </ScrollView>
       </Container>
     );
   }
