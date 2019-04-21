7692537b56948e3da071e7ad906516dc621f7563
castrodrigo <castrodrigo@yahoo.com.br>
2019-04-22 01:29:58 +0200

    Add storage integration for cards data

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import { AsyncStorage } from "react-native";
 
 const DECK_STORAGE_KEY = "flashcards-app:deck";
+const CARD_STORAGE_KEY = "flashcards-app:card";
 
 export function getDecks() {
   return AsyncStorage.getItem(DECK_STORAGE_KEY);
@@ -11,5 +12,17 @@ export const addDeck = ({ deck, key }) =>
     DECK_STORAGE_KEY,
     JSON.stringify({
       [key]: deck
+    })
+  );
+
+export function getCards() {
+  return AsyncStorage.getItem(CARD_STORAGE_KEY);
+}
+
+export const addCard = ({ card, key }) =>
+  AsyncStorage.mergeItem(
+    CARD_STORAGE_KEY,
+    JSON.stringify({
+      [key]: card
     })
   );
